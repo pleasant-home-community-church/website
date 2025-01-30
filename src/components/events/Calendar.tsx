1583f2fc41790c4fc970fe849fb0ac2f7ef10ba2
Ben Wart <benwart@gmail.com>
2025-01-29 20:43:23 -0800

    feat: theme support in calendar

--- src/components/events/Calendar.tsx
+++ src/components/events/Calendar.tsx
@@ -9,14 +9,39 @@ import { createEventModalPlugin } from '@schedule-x/event-modal';
 import { createCurrentTimePlugin } from '@schedule-x/current-time';
  
 import '~/assets/styles/calendar.css';
+
+function matchSelector(selector) {
+  const matches = typeof selector === 'string' ? document.querySelectorAll(selector) : selector;
+  if (matches && matches.length) {
+    return matches
+  }
+}
+
+function attachEvent(matches, event, fn) {
+  if (matches && matches.length) {
+    matches.forEach((elem) => {
+      elem.addEventListener(event, fn, false);
+    });
+  }
+}
+
+function removeEvent(matches, event, fn) {
+  if (matches && matches.length) {
+    matches.forEach((elem) => {
+      elem.removeEventListener(event, fn, false);
+    });
+  }
+}
  
 function CalendarApp({categories, events, minDate, maxDate}) { 
   const eventsService = useState(() => createEventsServicePlugin())[0]
+
+  
   const calendar = useCalendarApp({
     firstDayOfWeek: 0,
     minDate,
     maxDate,
-    isDark: false,
+    isDark: document.documentElement.classList.contains('dark'),
     views: [
       createViewMonthGrid(), 
       createViewMonthAgenda(),
@@ -31,8 +56,22 @@ function CalendarApp({categories, events, minDate, maxDate}) {
   })
 
   useEffect(() => {
-    // get all events
     eventsService.getAll()
+  }, [])
+
+  const handleThemeChange = () => {
+    const dark = document.documentElement.classList.contains('dark');
+    calendar.setTheme(dark ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    const matches = matchSelector('[data-aw-toggle-color-scheme]');
+    attachEvent(matches, 'click', handleThemeChange);
+
+    // cleanup this component
+    return () => {
+      removeEvent(matches, 'click', handleThemeChange);
+    };  
   }, [])
  
   return (

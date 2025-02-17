c5deff88eb0dea133fe1fc5b8efc5eeec033263d
Ben Wart <benwart@gmail.com>
2025-02-17 10:26:00 -0800

    feat: add robots.txt and update sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -24,6 +24,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  site: 'https://www.phccgresham.org',
 
   experimental: {
     svg: true,

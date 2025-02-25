71839e86bb3e5b93af7b2d4ca192a2a7e385c6fe
Ben Wart <benwart@gmail.com>
2025-02-24 16:34:43 -0800

    feat: use church center as cms for ministries

--- scripts/planningcenter_fetch_ministries.py
+++ scripts/planningcenter_fetch_ministries.py
@@ -1,16 +1,21 @@
 from argparse import ArgumentParser, Namespace
-from datetime import datetime, timedelta, UTC
 from os import environ
+from re import compile, Pattern
 from shutil import rmtree
-from typing import Callable, AsyncIterator
+from typing import Callable, AsyncIterator, Optional
 
 from anyio import run, Path
 from decorest import backend, content, endpoint, on, query, GET, RestClient
 from dotenv import load_dotenv
-from httpx import BasicAuth
+from httpx import BasicAuth, AsyncClient
 from loguru import logger
+from markdownify import MarkdownConverter
 
-from planningcenter_models_ministries import PageInstance
+from planningcenter_models_ministries import (
+    ImageBlock,
+    PageInstance,
+    TextBlock,
+)
 
 
 CC_TO_SITE_SLUGS: dict[str, str] = {
@@ -60,13 +65,83 @@ class Publishing(RestClient):
     ): ...
 
 
-async def save_first_image(page: PageInstance, images_dir: Path) -> str:
-    return "testing.jpg"
+async def download_images(page: PageInstance, images_dir: Path) -> tuple[Optional[str], dict[str, str]]:
+    slug: str = CC_TO_SITE_SLUGS[page.attr.slug]
+    first: Optional[str] = None
+    images: dict[str, str] = {}
+
+    # look through all the blocks
+    for block in page.attr.blocks:
+        match block:
+            case ImageBlock():
+                if not first:
+                    first = block.id
+
+                # make image file path
+                suffix: str = Path(block.alt).suffix
+                image_file: Path = images_dir / f"ministry-{slug}-{block.id}{suffix}"
+                images[block.id] = image_file.name
+
+                # download all the images from each page
+                async with AsyncClient() as client:
+                    async with client.stream("GET", block.src) as response:
+                        async with await image_file.open("wb") as f:
+                            async for chunk in response.aiter_bytes():
+                                await f.write(chunk)
+
+    # return the first image id and all the images by id
+    return first, images
+
+
+async def geneate_excerpt(markdown: str) -> str:
+    return f"{markdown[0:100]}..."
+
+
+MD_LINK: Pattern = compile(r"\[(?P<text>[^\]]+)\]\((?P<src>[^\)]+)\)")
+
+
+class BlankTargetLinkConverter(MarkdownConverter):
+    """
+    Create a custom MarkdownConverter that adds target _blank to links
+    """
+
+    def convert_a(self, el, text, parent_tags):
+        md_link: str = super().convert_a(el, text, parent_tags)
+        m = MD_LINK.match(md_link)
+        if m:
+            g = m.groupdict()
+            return f'<a href="{g["src"]}" target="_blank">{g["text"]}</a>'
+        else:
+            return f"{md_link}"
+
+
+# Create shorthand method for conversion
+def md(html, **options):
+    return BlankTargetLinkConverter(**options).convert(html)
+
+
+async def convert_content(page: PageInstance) -> tuple[str, str]:
+    content: list[str] = []
+    excerpt: Optional[str] = None
+
+    # iterate blocks and render content
+    for block in page.attr.blocks:
+        match block:
+            case TextBlock():
+                markdown: str = md(block.content)
+                content.append(f"{markdown}")
+
+    # generate excerpt
+    combined: str = "\n".join(content)
+    excerpt: str = await geneate_excerpt(combined)
+
+    return excerpt.replace("\n", ""), combined
 
 
 async def model_to_markdown(page: PageInstance, ministries_dir: Path, images_dir: Path):
     # fetch the header image from church center and store it locally all other images will be left on church center
-    image_file_name: str = await save_first_image(page, images_dir)
+    first_id, images = await download_images(page, images_dir)
+    excerpt, content = await convert_content(page)
 
     # compute the minstry markdown file name
     ministry_file: Path = ministries_dir / f"{CC_TO_SITE_SLUGS[page.attr.slug]}.md"
@@ -75,12 +150,15 @@ async def model_to_markdown(page: PageInstance, ministries_dir: Path, images_dir
         await f.writelines(
             [
                 "---\n",
-                f'title: "Children\'s Ministry"\n',
-                # "excerpt: Serving Children and Parents\n",
-                f"image: ~/assets/images/{image_file_name}\n",
+                f'title: "{page.attr.title}"\n',
+                f"excerpt: {excerpt}\n",
+                f"image: ~/assets/images/{images[first_id]}\n",
                 "---\n\n",
             ]
         )
+
+        # write the content
+        await f.write(content)
 
         # make sure we flush the data to the file
         await f.flush()
--- src/data/ministries/counseling.md
+++ src/data/ministries/counseling.md
@@ -1,9 +1,11 @@
 ---
-title: Counseling
-excerpt: Our desire is to help you glorify God in your thoughts, words and actions as you walk through life and interact with others.
-image: ~/assets/images/ministry-counseling-header.webp
+title: "Counseling"
+excerpt: Our desire is to help you glorify God in your thoughts, words and actions as you walk through life a...
+image: ~/assets/images/ministry-counseling-825206bb-1a43-40df-acf5-93f223b38e28.webp
 ---
 
+Our desire is to help you glorify God in your thoughts, words and actions as you walk through life and interact with others.  
+  
 We are confident that through the Scriptures and the power of His Spirit, God has given us everything we need for life and godliness (2 Peter 1:3-4). Because of this, the Scriptures are held as the final authority in all matters.
 
-[Login](https://biblicare.net/Site/Access/Login?KeyID=EBCAC141-1224-490B-B7F5-35027166)
+<a href="https://biblicare.net/Site/Access/Login?KeyID=EBCAC141-1224-490B-B7F5-35027166" target="_blank">Login</a>
